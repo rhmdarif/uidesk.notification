b84d4da64da3792a63e787ed87b2b54855c01065
Rahmad Arif <rahmadarif28080@gmail.com>
2022-06-08 22:09:20 +0700

    add socket bri

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ app.post("/to-socket", express.json(), async (req, res) => {
     res.status(200).json({status: true, msg: "Data telah diteruskan"})
 });
 
+app.post("/to-socket-bri", express.json(), async (req, res) => {
+    const { HeaderID, LevelUser, UserName, StatusTransaksiType } = req.body;
+    httpSocket.emit("event-bri", HeaderID, req.body);
+    httpsSocket.emit("event-bri", HeaderID, req.body);
+
+    res.status(200).json({status: true, msg: "Data telah diteruskan"})
+});
+
+httpSocket.on('to-socket-bri', (data) => {
+    httpSocket.emit("event-bri", data.HeaderID, data);
+});
+
+httpsSocket.on('to-socket-bri', (data) => {
+    httpsSocket.emit("event-bri", data.HeaderID, data);
+});
+
 
 const PORT_HTTP = 80;
 httpServer.listen(PORT_HTTP, () => {
